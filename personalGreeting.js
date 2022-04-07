5f79dd28ac440aa5c6c3a3434583691fe09c2fdf
AtYourServas <joeservas@gmail.com>
2022-04-07 16:57:03 -0400

    adding 2022-04-07 codewar challenges

---
+++ personalGreeting.js
@@ -0,0 +1,9 @@
+/* Make a function that will return a greeting statement that uses an input; 
+your program should return, "Hello, <name> how are you doing today?". */
+function greet(name){
+    return `Hello, ${name} how are you doing today?`
+  }
+
+  //Ex
+
+  greet(Ryan)  //should return 'Hello, Ryan how are you doing today?
