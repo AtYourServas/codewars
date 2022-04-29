06b6bcf76b538dbf2f9a0d6df66fc3940102df2f
AtYourServas <joeservas@gmail.com>
2022-04-28 20:37:06 -0400

    adding 2022-04-28 code war challenges

---
+++ binToDec.js
@@ -0,0 +1,15 @@
+/* Complete the function which converts a binary number (given as a string) to a decimal number. */
+
+function binToDec(bin){
+    //create variable to track value
+    let result = 0
+    //iterate thru each binary value in the string
+    for (let i = 0; i < bin.length; i++) {
+        //calc value for that value based on its place
+        result += Number(bin[i]) * (2 ** (bin.length - i - 1))
+    }
+    return result
+  }
+
+  //Test
+console.log(binToDec('1001001'), 73)
