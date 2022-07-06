b45a439428610aa713ab47ba8f99f03235112dd3
AtYourServas <joeservas@gmail.com>
2022-07-06 17:28:37 -0400

    adding rooms.js

---
+++ rooms.js
@@ -0,0 +1,20 @@
+/* You are creating an "Escape the room" game. The first step is to create a hash table called rooms that contains all of the rooms of the game. There should be at least 3 rooms inside it, each being a hash table with at least three properties (e.g. name, description, completed). */
+
+// Add rooms here
+var rooms = {
+    casino: {
+      name: 'casino',
+      description: 'casino',
+      completed: false
+    },
+  jail: {
+      name: 'jail',
+      description: 'jail',
+      completed: false
+    },
+  school: {
+      name: 'school',
+      description: 'school',
+      completed: false
+    }
+  }
