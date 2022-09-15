724f20ca9b7ba599030209fb9c90ffc3c4b07917
AtYourServas <joeservas@gmail.com>
2022-09-14 21:51:19 -0400

    adding 2022-09-14 codewar challenge

---
+++ removeSmallest.js
@@ -0,0 +1,35 @@
+/* The museum of incredible dull things
+The museum of incredible dull things wants to get rid of some exhibitions. Miriam, the interior architect, comes up with a plan to remove the most boring exhibitions. She gives them a rating, and then removes the one with the lowest rating.
+
+However, just as she finished rating all exhibitions, she's off to an important fair, so she asks you to write a program that tells her the ratings of the items after one removed the lowest one. Fair enough.
+
+Task
+Given an array of integers, remove the smallest value. Do not mutate the original array/list. If there are multiple elements with the same value, remove the one with a lower index. If you get an empty array/list, return an empty array/list.
+
+Don't change the order of the elements that are left.
+
+Examples
+* Input: [1,2,3,4,5], output= [2,3,4,5]
+* Input: [5,3,2,1,4], output = [5,3,2,4]
+* Input: [2,2,1,2,1], output = [2,2,2,1] */
+
+// PARAMETERS:  an array of integers or an empty array
+// RETURN:  a copy of the same array with the smallest value removed.  if the smallest value is duplicated, only one instance should be removed (the one with the lowest index)
+// EXAMPLES:
+// Input: [1,2,3,4,5], output= [2,3,4,5]
+// Input: [5,3,2,1,4], output = [5,3,2,4]
+// Input: [2,2,1,2,1], output = [2,2,2,1]  
+
+// PSEUDOCODE:  create a copy of the array using slice, find the minimum value of the array, find the first index of the minimum, splice out the first instance of the minimum value, return the array
+
+function removeSmallest(numbers) {
+    if (numbers.length) {
+      let result = numbers.slice()
+      let indexOfMin = numbers.indexOf(Math.min(...numbers))
+      result.splice(indexOfMin, 1)
+      return result
+    }
+    else {
+      return []
+    }
+  }
