1da7a05ae9b8c7c3ed38ef33a593acdd7995136a
AtYourServas <joeservas@gmail.com>
2022-10-04 17:22:48 -0400

    adding 2022-10-04 codewar challenge

---
+++ maxTriSum.js
@@ -0,0 +1,38 @@
+/* Task
+Given an array/list [] of n integers , find maximum triplet sum in the array Without duplications .
+
+Notes :
+Array/list size is at least 3 .
+
+Array/list numbers could be a mixture of positives , negatives and zeros .
+
+Repetition of numbers in the array/list could occur , So (duplications are not included when summing).
+
+Input >> Output Examples
+1- maxTriSum ({3,2,6,8,2,3}) ==> return (17)
+Explanation:
+As the triplet that maximize the sum {6,8,3} in order , their sum is (17)
+
+Note : duplications are not included when summing , (i.e) the numbers added only once .
+
+2- maxTriSum ({2,1,8,0,6,4,8,6,2,4}) ==> return (18)
+Explanation:
+As the triplet that maximize the sum {8, 6, 4} in order , their sum is (18) ,
+
+Note : duplications are not included when summing , (i.e) the numbers added only once .
+
+3- maxTriSum ({-7,12,-7,29,-5,0,-7,0,0,29}) ==> return (41)
+Explanation:
+As the triplet that maximize the sum {12 , 29 , 0} in order , their sum is (41) ,
+
+Note : duplications are not included when summing , (i.e) the numbers added only once . */
+
+// PARAMETERS:  an array of integers with length >= 3
+// RETURN:  highest possible sum of three unique integers within the provided array
+// EXAMPLES:  see above
+// PSEUDOCODE:  create a set of unique values and have it sorted from lowest to highest
+//              sum the last three values of the array
+
+function maxTriSum(numbers){
+    return ([...new Set(numbers)]).sort((a,b) => a-b).slice(-3).reduce((a,b) => a+b);
+  }
