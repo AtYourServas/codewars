4f693a36d383d698106c203d93c385a43c71ca85
AtYourServas <joeservas@gmail.com>
2023-04-11 22:13:57 -0400

    adding 2022-04-11 codewar challenge

---
+++ anotherLongest.js
@@ -0,0 +1,10 @@
+/* a = "xyaabbbccccdefww"
+b = "xxxxyyyyabklmopq"
+longest(a, b) -> "abcdefklmopqwxy"
+
+a = "abcdefghijklmnopqrstuvwxyz"
+longest(a, a) -> "abcdefghijklmnopqrstuvwxyz" */
+
+function longest(s1, s2) {
+    return (s1, s2) => [...new Set(s1+s2)].sort().join('')
+  }
